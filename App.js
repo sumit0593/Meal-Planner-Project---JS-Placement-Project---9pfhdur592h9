6923c7ca876283fc2dc834232849dc99b1125929
sumit0593 <sumit0593>
2023-07-19 10:51:38 +0530

    new push

--- App.js
+++ App.js
@@ -1,153 +1,165 @@
-"use Strict";
-const height = document.getElementById("height");
-const weight = document.getElementById("weight");
-const age = document.getElementById("age");
-const gender = document.getElementById("gender");
-const activity = document.getElementById("activity");
-const generateMealBtn = document.getElementById("btn");
-const box_1 = document.getElementsByClassName("box1");
-const box_2 = document.getElementsByClassName("box2");
-const box_3 = document.getElementsByClassName("box3");
-const recipeBtn = document.getElementById("button");
-const card = document.querySelector(".card-box");
-const ingrediants = document.querySelector(".ingrediants");
-const equipment = document.querySelector(".equipment");
-const steps = document.querySelector(".steps");
-const tbody = document.querySelector("tbody"); ///
-const load = document.querySelector("#load");
-const bgSection = document.querySelector(".bg");
-const recipeBg = document.querySelector(".recipe");
 
-// api Keys
-const apiKey = "9cbf9c768e724bbb975fdac892698efd";
 
-// Initial Meal Data
-async function initialMeal() {
-  const url = `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&timeFrame=day`;
-  const api = await fetch(url);
-  const resp = await api.json();
-  return resp; //meal[2]
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const height = document.getElementById("height");
+  const weight = document.getElementById("weight");
+  const age = document.getElementById("age");
+  const gender = document.getElementById("gender");
+  const activity = document.getElementById("activity");
+  const mealsPerDay = document.getElementById("meals-per-day");
+  const generateMealPlanBtn = document.getElementById("sub-btn");
+  const mealPlanContainer = document.getElementById("meal-plan");
+  const recipeModal = document.getElementById("recipe-modal");
+  const recipeTitle = document.getElementById("recipe-title");
+  const recipeInstructions = document.getElementById("recipe-instructions");
+  const closeModal = document.getElementsByClassName("close")[0];
+  const homeIcon = document.getElementById("home-icon");
 
-async function generateInitialMeal() {
-  const data = await initialMeal();
-  await mealData(data.meals);
-}
-generateInitialMeal();
-
-//Calculating the calories of the user
-async function mealColories() {
-  let bmrMale =
-    66.47 + 13.75 * weight.value + 5.003 * height.value - 6.755 * age.value;
-  let bmrFemale =
-    655.1 + 9.563 * weight.value + 1.85 * height.value - 4.676 * age.value;
-  if (gender.value === "male" && activity.value === "light") {
-    var calories = bmrMale * 1.375;
-  } else if (gender.value === "male" && activity.value === "moderate") {
-    var calories = bmrMale * 1.55;
-  } else if (gender.value === "male" && activity.value === "active") {
-    var calories = bmrMale * 1.725;
-  } else if (gender.value === "female" && activity.value === "light") {
-    var calories = bmrFemale * 1.375;
-  } else if (gender.value === "female" && activity.value === "moderate") {
-    var calories = bmrFemale * 1.55;
-  } else if (gender.value === "female" && activity.value === "active") {
-    var calories = bmrFemale * 1.725;
+  const loadingBar = document.getElementById("loading-bar");
+  const loadingBarProgress = document.getElementById("loading-bar-progress");
+  const loadingBarText = document.getElementById("loading-bar-text");
+  
+ // function to show loading bar
+  function showLoadingBar() {
+    loadingBar.style.display = "block";
+    loadingBarProgress.style.width = "0%";
+    loadingBarText.textContent = "Loading...";
   }
-  const url = `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&timeFrame=day&targetCalories=${calories}`;
-  const resp = await fetch(url);
-  const respData = await resp.json();
-  return respData;
-}
 
-async function mealData(data) {
-  card.innerHTML = " ";
-  ingrediants.innerHTML = " ";
-  equipment.innerHTML = " ";
-  steps.innerHTML = " ";
-  data.map(async (i) => {
-    const mealUrl = `https://api.spoonacular.com/recipes/${i.id}/information?apiKey=${apiKey}&includeNutrition=false`;
-    const respMeal = await fetch(mealUrl);
-    const res = await respMeal.json();
-    load.style.display = "block";
-    setTimeout(() => {
-      load.style.display = "None";
-      generateHTML(res);
-    }, 1500);
+  // Function to update the loading bar progress
+  function updateLoadingBar(progress) {
+    loadingBarProgress.style.width = `${progress}%`;
+  }
+
+  // Function to hide the loading bar
+  function hideLoadingBar() {
+    loadingBar.style.display = "none";
+  }
+
+  const apiKey = "9cbf9c768e724bbb975fdac892698efd";
+
+  function calculateCalories() {
+    let bmrMale =
+      66.47 + 13.75 * weight.value + 5.003 * height.value - 6.755 * age.value;
+    let bmrFemale =
+      655.1 + 9.563 * weight.value + 1.85 * height.value - 4.676 * age.value;
+
+    let calories = 0;
+
+    if (gender.value === "male" && activity.value === "light") {
+      calories = bmrMale * 1.375;
+    } else if (gender.value === "male" && activity.value === "moderate") {
+      calories = bmrMale * 1.55;
+    } else if (gender.value === "male" && activity.value === "active") {
+      calories = bmrMale * 1.725;
+    } else if (gender.value === "female" && activity.value === "light") {
+      calories = bmrFemale * 1.375;
+    } else if (gender.value === "female" && activity.value === "moderate") {
+      calories = bmrFemale * 1.55;
+    } else if (gender.value === "female" && activity.value === "active") {
+      calories = bmrFemale * 1.725;
+    }
+
+    return Math.floor(calories / mealsPerDay.value);
+  }
+
+  async function fetchMealPlan() {
+    showLoadingBar();
+
+    const caloriesPerMeal = calculateCalories();
+    const url = `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&timeFrame=day&targetCalories=${caloriesPerMeal}&diet=balanced&number=${mealsPerDay.value}`;
+
+    try {
+      const response = await fetch(url);
+
+      // Update loading bar progress to 50%
+      updateLoadingBar(50);
+
+      const data = await response.json();
+      renderMealPlan(data);
+
+      // Hide loading bar when meal plan is rendered
+      hideLoadingBar();
+    } catch (error) {
+      console.log("Error fetching meal plan:", error);
+      // Hide loading bar if there's an error
+      hideLoadingBar();
+    }
+  }
+
+  async function fetchMealImage(mealId) {
+    const url = `https://api.spoonacular.com/recipes/${mealId}/information?apiKey=${apiKey}&includeNutrition=false`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.image;
+  }
+
+  async function renderMealPlan(data) {
+    mealPlanContainer.innerHTML = "";
+  
+    for (const meal of data.meals) {
+      const mealCard = document.createElement("div");
+      mealCard.className = "meal-card";
+  
+      const caloriesPerMeal = calculateCalories();
+  
+      const mealImage = await fetchMealImage(meal.id);
+  
+      mealCard.innerHTML = `
+        <h3>${meal.title}</h3>
+        <div class="img-loaded" style="background-color: lightgreen;">
+          <img src="${mealImage}" alt="${meal.title} Image">
+        </div>
+        <p>Calories: ${caloriesPerMeal !== undefined ? `${Math.floor(caloriesPerMeal)} kcal` : "N/A"}</p>
+        <button class="view-recipe-btn" data-id="${meal.id}">View Recipe</button>
+      `;
+      mealPlanContainer.appendChild(mealCard);
+    }
+  
+    const viewRecipeBtns = document.getElementsByClassName("view-recipe-btn");
+    Array.from(viewRecipeBtns).forEach((btn) => {
+      btn.addEventListener("click", handleViewRecipe);
+    });
+  }
+  
+  
+  
+  
+
+  async function handleViewRecipe(event) {
+    const mealId = event.target.dataset.id;
+    const url = `https://api.spoonacular.com/recipes/${mealId}/information?apiKey=${apiKey}&includeNutrition=false`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      showRecipeModal(data);
+    } catch (error) {
+      console.log("Error fetching recipe:", error);
+    }
+  }
+
+  function showRecipeModal(recipe) {
+    recipeTitle.textContent = recipe.title;
+    recipeInstructions.innerHTML = `
+      <h3>Instructions:</h3>
+      <ol>
+        ${recipe.instructions}
+      </ol>
+    `;
+    recipeModal.style.display = "block";
+  }
+
+  closeModal.addEventListener("click", function () {
+    recipeModal.style.display = "none";
   });
-}
-async function generateMeal() {
-  const data = await mealColories();
-  await mealData(data.meals);
-}
-generateMealBtn.addEventListener("click", generateMeal);
 
-// Generate Html Function
-function generateHTML(results) {
-  //hiding the Recipe Background
-  bgSection.style.display = "none";
-  recipeBg.style.display = "none";
-  //creating dynamic element into the DOM
-  const item = document.createElement("span");
-  const img = document.createElement("img");
-  const title = document.createElement("h3");
-  let getRecipeBtn = document.createElement("Button");
-  item.setAttribute("class", "grid");
-
-  //adding functionality to the Get Recipe button
-  function getRecipeData() {
-    //Showing the Recipe Background along with List element
-    bgSection.style.display = "block";
-    recipeBg.style.display = "block";
-
-    //Adding the ingredients
-    ingrediants.innerHTML = " ";
-    ingrediants.innerHTML = `<h2>Ingredients</h2>`;
-    let apiIngre = results.extendedIngredients;
-    for (let i = 0; i < apiIngre.length; i++) {
-      let para = document.createElement("li");
-      let newPara = apiIngre[i].original;
-      para.innerHTML = newPara;
-      ingrediants.appendChild(para);
-    }
-    //Adding the Equipments
-    equipment.innerHTML = " ";
-    equipment.innerHTML = `<h2>Equipment</h2>`;
-    for (let j = 0; j < results.analyzedInstructions.length; j++) {
-      let apiEqipment = results.analyzedInstructions[j].steps;
-      for (let i = 0; i < apiEqipment.length; i++) {
-        let apiEqipment2 = apiEqipment[i].equipment;
-        for (let k = 0; k < apiEqipment2.length; k++) {
-          let para = document.createElement("li");
-          let newPara = apiEqipment2[k].name;
-          para.innerHTML = newPara;
-          equipment.appendChild(para);
-        }
-      }
-    }
-
-    //Adding the Steps
-    steps.innerHTML = " ";
-    steps.innerHTML = `<h2>Steps</h2>`;
-    let ol = document.createElement("ol");
-    for (let j = 0; j < results.analyzedInstructions.length; j++) {
-      let apiStep = results.analyzedInstructions[j].steps;
-      for (let i = 0; i < apiStep.length; i++) {
-        let para = document.createElement("li");
-        let newPara = apiStep[i].step;
-        para.innerHTML = newPara;
-        ol.appendChild(para);
-        steps.appendChild(ol);
-      }
-    }
-  }
-  getRecipeBtn.setAttribute("class", "get-btn");
-  getRecipeBtn.addEventListener("click", getRecipeData);
-  img.setAttribute("src", results.image);
-  title.innerHTML = results.title;
-  getRecipeBtn.innerHTML = "Get Receipe";
-  item.appendChild(img);
-  item.appendChild(title);
-  item.appendChild(getRecipeBtn);
-  card.appendChild(item);
-}
+  generateMealPlanBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    fetchMealPlan();
+  });
+  
+  homeIcon.addEventListener("click", function () {
+    location.reload();
+  });
+});
